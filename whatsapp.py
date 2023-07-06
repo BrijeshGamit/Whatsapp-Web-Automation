3b7e507cba547838367c71dac0cbdd85ce628988
BrijeshGamit <brijeshgamit66@gmail.com>
2023-07-06 22:49:02 +0530

    new whatsp changes

--- whatsapp.py
+++ whatsapp.py
@@ -10,8 +10,8 @@ chrome_options.add_experimental_option("debuggerAddress","127.0.0.1:9222")
 
 #user_name = "Mummy"
 #user_name = "Apne😊😇😊"
-#user_name = "ToDo list"
-user_name = "Nitin Bhai"
+user_name = "ToDo list"
+#user_name = "Nitin Bhai"
 #msg = "Good night \n"
 
 
@@ -29,12 +29,12 @@ def printit():
 	files=os.listdir(path)
 	d=random.choice(files)
 	file = "C:/python/webauto/imgs/"+d
-	media = driver.find_element_by_css_selector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._3HQNh._1un-p > div._2jitM > div > span > div > div > ul > li:nth-child(1)")
+	media = driver.find_element_by_css_selector("#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._2xy_p._1bAtO > div._1OT67 > div > span > div > div > ul > li:nth-child(1) > button")
 	media.click()
-	input_file = driver.find_element_by_css_selector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._3HQNh._1un-p > div._2jitM > div > span > div > div > ul > li:nth-child(1) > button > input[type=file]")
+	input_file = driver.find_element_by_css_selector("#main > footer > div._2lSWV._3cjY2.copyable-area > div > span:nth-child(2) > div > div._2xy_p._1bAtO > div._1OT67 > div > span > div > div > ul > li:nth-child(1) > button > input[type=file]")
 	input_file.send_keys(file)
 	driver.implicitly_wait(1000)
-	send_btn = driver.find_element_by_css_selector("#app > div > div > div._3ArsE > div.ldL67._3sh5K > span > div > span > div > div > div.KPJpj._2M_x0 > div > div._1HI4Y > div._33pCO > div")
+	send_btn = driver.find_element_by_css_selector("#app > div > div > div._2QgSC > div._2Ts6i._2xAQV > span > div > span > div > div > div.g0rxnol2.thghmljt.p357zi0d.rjo8vgbg.ggj6brxn.f8m0rgwh.gfz4du6o.r7fjleex.bs7a17vp > div > div.O2_ew > div._3wFFT > div > div > span")
 	send_btn.click()
 	time.sleep(5)
 	printit()
